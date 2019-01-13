9395709cb1fd3bded835d7f52ba9b935a7eed2f9
Moses Finlay <islandmoses@gmail.com>
2019-01-13 15:53:02 -0800

    Add comments

--- app.js
+++ app.js
@@ -4,32 +4,39 @@ const app = express();
 const port = 3000;
 const { projects } = require("./data.json");
 
+// You can access anything in the public folder via "/static"
 app.use("/static", express.static("public"));
+// You can render the pug templates use the filename without ".pug"
 app.set("view engine", "pug");
 
+// Home route
 app.get("/", (req, res) => {
   res.locals.projects = projects;
-  res.render("index.pug", projects);
+  res.render("index", projects);
 });
 
+// About route
 app.get("/about", (req, res) => {
-  res.render("about.pug");
+  res.render("about");
 });
 
+// Projects route
 app.get("/projects/:id", (req, res) => {
+  // Project JSON to be passed to the "project" template
   res.locals.projects = projects[req.params.id];
-  res.render("project.pug", projects);
+  res.render("project", projects);
 });
 
+// If the route requested does not exist show an error
 app.use((req, res, next) => {
   const err = new Error("Not Found");
   err.status = 404;
   console.log(`There was an error. (${err.message})`);
-  res.render("error.pug", { err });
-  console.dir(err);
+  res.render("error", { err });
   next(err);
 });
 
+// Listens to the port variable
 app.listen(port, () => {
   console.log(`The portfolio site is running at localhost:${port}`);
 });
